9989836f5333ed04b4d9d1030de37f0b7a1fad4d
houda <houda.mtq2@gmail.com>
2022-12-23 12:54:56 +0100

    Initial_Commit

---
+++ src/main/java/com/example/controlejee/dtos/MatchDTORequest.java
@@ -0,0 +1,20 @@
+package com.example.controlejee.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatchDTORequest {
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date date;
+    private String lieu;
+    private String equipe1;
+    private String equipe2;
+    private Integer ticketsDispo;
+}
